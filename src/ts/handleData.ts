22669ad3a26c344700fd14c4516214920a6ee68d
bansheerubber <gary.cactus.email@gmail.com>
2019-01-19 15:43:53 -0700

    push

--- src/ts/handleData.ts
+++ src/ts/handleData.ts
@@ -3,20 +3,20 @@ import { Pane3D } from "./3d";
 
 interface SensorData {
 	gas: {
-		eco2: number,
-		tvoc: number
+		eco2: number, // parts per million
+		tvoc: number // parts per billion 
 	},
 	orientation: {
 		x: number, // yaw
-		y: number, // pitch?
-		z: number // roll?
+		y: number, // pitch
+		z: number // roll
 	},
 	proximity: {
-		proximity: number,
-		ambientlight: number
+		proximity: number, // range from 0-2^16
+		ambientlight: number // range from 0-2^16
 	},
 	temperature: {
-		temperature: number
+		temperature: number // celcius
 	},
 	timestamp: number
 }
@@ -24,4 +24,17 @@ interface SensorData {
 export default function handleData(json: SensorData, temperature: any[][], thermalCamera: ThermalCamera, pane3d: Pane3D) {
 	pane3d.setBoardRotation(json.orientation.y * Math.PI / 180, json.orientation.x * Math.PI / 180, json.orientation.z * Math.PI / 180)
 	thermalCamera.setThermalData(temperature)
+
+	// raw ass javascript because i'm a criminal
+	let tempValue = Math.ceil((4 - 1) / (35 - 20) * json.temperature.temperature - 4)
+	let flameValue = Math.ceil((2 - 1) / (200 - 50) * json.gas.tvoc - 1 / 3)
+	let cloudValue = Math.floor((2 - 1) / (430 - 400) * json.gas.eco2 - 12.3333333333)
+	let lightbulbValue = Math.ceil((5 - 1) / (Math.pow(2, 16) - 0) * (json.proximity.proximity + Math.pow(2, 16) / 10))
+	let proximityValue = Math.ceil((3 - 1) / (Math.pow(2, 16) - 0) * (json.proximity.proximity + Math.pow(2, 16) / 10))
+
+	document.getElementById("temp").style.backgroundImage = `url(./data/icons/temp${tempValue}.png)`
+	document.getElementById("flame").style.backgroundImage = `url(./data/icons/flame${flameValue}.png)`
+	document.getElementById("cloud").style.backgroundImage = `url(./data/icons/cloud${cloudValue}.png)`
+	document.getElementById("lightbulb").style.backgroundImage = `url(./data/icons/lightbulb${lightbulbValue}.png)`
+	document.getElementById("proximity").style.backgroundImage = `url(./data/icons/proximity${proximityValue}.png)`
 }
