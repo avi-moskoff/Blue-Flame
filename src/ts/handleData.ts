b3f870123e67e579dc7bdb87211420dac1417cf8
bansheerubber <gary.cactus.email@gmail.com>
2019-01-19 17:01:21 -0700

    finished changing theme

--- src/ts/handleData.ts
+++ src/ts/handleData.ts
@@ -26,15 +26,17 @@ export default function handleData(json: SensorData, temperature: any[][], therm
 	thermalCamera.setThermalData(temperature)
 
 	// raw ass javascript because i'm a criminal
-	let tempValue = Math.ceil((4 - 1) / (35 - 20) * json.temperature.temperature - 4)
-	let flameValue = Math.ceil((2 - 1) / (200 - 50) * json.gas.tvoc - 1 / 3)
+	let tempValue = Math.ceil((5 - 1) / (35 - 20) * json.temperature.temperature - 4)
+	let flameValue = Math.ceil((3 - 1) / (200 - 50) * json.gas.tvoc - 1 / 3)
 	let cloudValue = Math.floor((2 - 1) / (430 - 400) * json.gas.eco2 - 12.3333333333)
 	let lightbulbValue = Math.ceil((5 - 1) / (Math.pow(2, 16) - 0) * (json.proximity.proximity + Math.pow(2, 16) / 10))
 	let proximityValue = Math.ceil((3 - 1) / (Math.pow(2, 16) - 0) * (json.proximity.proximity + Math.pow(2, 16) / 10))
 
-	document.getElementById("temp").style.backgroundImage = `url(./data/icons/temp${tempValue}.png)`
-	document.getElementById("flame").style.backgroundImage = `url(./data/icons/flame${flameValue}.png)`
-	document.getElementById("cloud").style.backgroundImage = `url(./data/icons/cloud${cloudValue}.png)`
-	document.getElementById("lightbulb").style.backgroundImage = `url(./data/icons/lightbulb${lightbulbValue}.png)`
-	document.getElementById("proximity").style.backgroundImage = `url(./data/icons/proximity${proximityValue}.png)`
+	console.log(tempValue, flameValue, cloudValue, lightbulbValue, proximityValue)
+
+	document.getElementById("temp").style.backgroundImage = `url(./data/icons2/temp${tempValue}.png)`
+	document.getElementById("flame").style.backgroundImage = `url(./data/icons2/flame${flameValue}.png)`
+	document.getElementById("cloud").style.backgroundImage = `url(./data/icons2/cloud${cloudValue}.png)`
+	document.getElementById("lightbulb").style.backgroundImage = `url(./data/icons2/lightbulb${lightbulbValue}.png)`
+	document.getElementById("proximity").style.backgroundImage = `url(./data/icons2/proximity${proximityValue}.png)`
 }
